34e0d73495f92a1252e106c3a7c46ddb372785c2
adam350 <56835153+adam350@users.noreply.github.com>
2024-05-28 22:03:38 +0400

    Create first.py

---
+++ Other/ml/first.py
@@ -0,0 +1,81 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+class LayerDense:
+    def __init__(self, InputNum, NeuronNum):
+        self.weights = 0.10 * np.random.randn(InputNum, NeuronNum)
+        self.biases = np.zeros((1, NeuronNum))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class ActivationReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class ActivationSoftMax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+class Loss:
+    def calculate(self, output, y):
+        sampleLoss = self.forward(output, y)
+        dataLoss = np.mean(sampleLoss)
+
+        return dataLoss
+    
+# CCE = Categorical Cross Entropy
+
+class CCE(Loss):
+    def forward(self, yPred, yTrue):
+        samples = len(yPred)
+
+        yPredClipped = np.clip(yPred, 1e-7, 1 - 1e-7)
+
+        if len(yTrue.shape) == 1:
+            correctConf = yPredClipped[
+                range(samples),
+                yTrue
+            ]
+        elif len(yTrue.shape) == 2:
+            correctConf = np.sum(
+                yPredClipped * yTrue,
+                axis=1
+            )
+
+        negativeLogProb = -np.log(correctConf)
+        return negativeLogProb
+
+
+X,y = spiral_data(samples=100, classes=3)
+
+layer1 = LayerDense(2,3)
+act1 = ActivationReLU()
+
+layer2 = LayerDense(3,3)
+act2 = ActivationSoftMax()
+
+lossF = CCE()
+
+layer1.forward(X)
+act1.forward(layer1.output)
+
+layer2.forward(act1.output)
+act2.forward(layer2.output)
+
+print(act2.output[:5])
+
+loss = lossF.calculate(act2.output, y)
+print("loss: ", loss)
+
+predictions = np.argmax(act2.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+accuracy = np.mean(predictions==y)
+
+print("acc: ", accuracy)
