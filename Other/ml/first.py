111ec68af594b8529bb3a66230349298382c5b9c
adam350 <56835153+adam350@users.noreply.github.com>
2024-05-28 22:39:36 +0400

    Update first.py

--- Other/ml/first.py
+++ Other/ml/first.py
@@ -2,6 +2,8 @@ import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
 
+import matplotlib.pyplot as plt
+
 nnfs.init()
 
 class LayerDense:
@@ -29,7 +31,7 @@ class Loss:
 
         return dataLoss
     
-# CCE = Categorical Cross Entropy
+# CCE = Categorical Cross E ntropy
 
 class CCE(Loss):
     def forward(self, yPred, yTrue):
@@ -68,14 +70,39 @@ act1.forward(layer1.output)
 layer2.forward(act1.output)
 act2.forward(layer2.output)
 
-print(act2.output[:5])
+# print(act2.output[:5])
 
-loss = lossF.calculate(act2.output, y)
-print("loss: ", loss)
+# loss = lossF.calculate(act2.output, y)
+# print("loss: ", loss)
 
-predictions = np.argmax(act2.output, axis=1)
-if len(y.shape) == 2:
-    y = np.argmax(y, axis=1)
-accuracy = np.mean(predictions==y)
+# predictions = np.argmax(act2.output, axis=1)
+# if len(y.shape) == 2:
+#     y = np.argmax(y, axis=1)
+# accuracy = np.mean(predictions==y)
 
-print("acc: ", accuracy)
+# print("acc: ", accuracy)
+
+def f(x):
+    return 2 * x ** 2
+
+x = np.arange(0, 5, 0.0001)
+y = f(x)
+
+plt.plot(x,y)
+
+p2_delta = 0.0001
+x1 = 2
+x2 = x1 + p2_delta
+
+y1 = f(x1)
+y2 = f(x2)
+
+Gradient = (y2 - y1) / (x2 - x1)
+b = y2 - Gradient*x2
+
+def tangent_Line(x):
+    return Gradient*x + b
+
+to_plot = [x1-0.9, x1, x1+0.9]
+plt.plot(to_plot, [tangent_Line[i] for i in to_plot])
+plt.show()
